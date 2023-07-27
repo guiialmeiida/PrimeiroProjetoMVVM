6030f4d9fd75287abba5593a1af039a41752b638
Guilherme Almeida <gui.almeidacrz@gmail.com>
2023-07-27 07:48:47 -0300

    Creating Screen for Post Cell

---
+++ PrimeiroProjetoMVVM/HomeVC/Cell/Post/PostCollectionViewCell/PostCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostCollectionViewCell.swift
+//  PrimeiroProjetoMVVM
+//
+//  Created by Guilherme Almeida on 27/07/23.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
